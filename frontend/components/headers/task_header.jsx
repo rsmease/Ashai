59d49eaeceda50c0ced87742f5835a966635e79c
Ryan Scott <rsmease@gmail.com>
2017-11-22 10:25:52 -0800

    Style main app headers

--- frontend/components/headers/task_header.jsx
+++ frontend/components/headers/task_header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link, Router } from 'react-router-dom';
+import TaskHeaderPrimary from './task_header_primary';
+import TaskHeaderSecondary from './task_header_secondary';
 
 export default  () => (
   <div className="task-header">
-    <h1>I am the header!</h1>
+    <TaskHeaderPrimary />
+    <TaskHeaderSecondary />
   </div>
 );
---
+++ frontend/components/headers/task_header_secondary.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link, Router } from 'react-router-dom';
+
+export default  () => (
+  <div className="task-header-secondary">
+    
+  </div>
+);

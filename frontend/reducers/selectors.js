fb944018fec6df7c02709be3066469e9fb92e0b9
Ryan Scott <rsmease@gmail.com>
2017-12-01 06:39:08 -0800

    Fix create/delete projects on sidebar

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -4,6 +4,16 @@ export const myTeamsArray = (teams, myTeamIds) => {
   return myTeamIds.map(id => teams[id]);
 };
 
-export const currentUserProjects(projects) {
+export const addUniqueToArray = (newItem, existingItems) => {
+  const itemIds = existingItems.map(item => item.id);
+  if (itemIds.indexOf(newItem.id) === -1) {
+    existingItems.push(newItem);
+  }
+  return existingItems;
+};
 
+export const removeDeleted = (removedItemId, existingItems) => {
+  return existingItems.filter(function(item) {
+    return item.id !== removedItemId;
+  });
 };
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -3,6 +3,16 @@ import {
   LOGOUT_CURRENT_USER,
 } from '../actions/session_actions';
 
+import {
+  RECEIVE_PROJECT,
+  REMOVE_PROJECT
+} from '../actions/project_actions';
+
+import {
+  addUniqueToArray,
+  removeDeleted
+} from './selectors';
+
 import _ from 'lodash';
 
 const _nullSession = {
@@ -10,11 +20,18 @@ const _nullSession = {
 };
 
 export default (state = _nullSession, action) => {
-  Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       const currentUser = action.user;
       return _.merge({}, state, { currentUser });
+    case RECEIVE_PROJECT:
+      addUniqueToArray(action.project, state.currentUser.projects);
+      return _.merge({}, state);
+    case REMOVE_PROJECT:
+      state.currentUser.projects =
+        removeDeleted(action.projectId, state.currentUser.projects);
+        console.log(state);
+      return _.merge({}, state);
     case LOGOUT_CURRENT_USER:
       return _nullSession;
     default:

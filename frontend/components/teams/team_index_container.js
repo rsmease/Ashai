a1312faf340836368f497aa390b3d7d998f3c8af
Ryan Mease <rsmease@gmail.com>
2017-12-29 00:15:25 -0600

    Delete outdated modals

--- frontend/components/teams/team_index_container.js
+++ frontend/components/teams/team_index_container.js
@@ -1,23 +1,30 @@
-import { connect } from 'react-redux';
-import { requestTeam } from '../../actions/team_actions';
-import { withRouter } from 'react-router-dom';
+import {
+    connect
+} from 'react-redux';
+import {
+    requestTeam
+} from '../../actions/team_actions';
+import {
+    withRouter
+} from 'react-router-dom';
 import TeamIndex from './team_index';
 
 
 const mapStateToProps = (state, ownProps) => {
 
-  let currentTeamId = ownProps.match.params.teamId;
+    let currentTeamId = ownProps.match.params.teamId;
 
-  return {
-    currentTeamId: currentTeamId,
-    currentUser: state.session.currentUser,
-    teamId: ownProps,
-    team: state.entities.teams[currentTeamId]
-  };
+    return {
+        currentTeamId: currentTeamId,
+        currentUser: state.session.currentUser,
+        teamId: ownProps,
+        team: state.entities.teams[currentTeamId]
+    };
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  requestTeam: (teamId) => dispatch(requestTeam(teamId))
+    requestTeam: (teamId) => dispatch(requestTeam(teamId))
 });
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TeamIndex));
+export default withRouter
+    (connect(mapStateToProps, mapDispatchToProps)(TeamIndex));
